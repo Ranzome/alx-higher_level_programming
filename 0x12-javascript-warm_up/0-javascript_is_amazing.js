aa7fb90fa13c3db043ce0fa137a75367b13c6af8
Ranzome <enebiransom@gmail.com>
2024-06-19 21:59:34 -0700

    js first

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-/*Write a script that prints “JavaScript is amazing”:*/
-const myVar = '0-javascript_is_amazing.js';
+/*Write a script that prints “JavaScript is amazing”*/
+const myVar = "0-javascript_is_amazing.js";
 console.log(myVar);
